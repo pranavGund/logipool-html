166eb73bbb920d097a9a214d57353a586bebd186
= <=>
2023-06-28 12:13:03 -0700

    commit file1

--- Dom/file1.js
+++ Dom/file1.js
@@ -1,3 +1,69 @@
-var element;
-element=document.querySelector("#selector").style.backgroundColor="yellow"
-console.log(element);
+//ElementnyId
+
+// var headerTitle=document.getElementById("header-title")
+// headerTitle.textContent="welcome To";
+// headerTitle.innerHTML="Welcome To infotech"
+// headerTitle.innerText="<h1>hello</h1>";
+// console.log(headerTitle);
+
+// var header=document.getElementById("header")
+// header.style.borderBottom="10px solid blue";
+
+
+// var items=document.getElementsByClassName("list-items");
+// console.log(items);
+// items[1].textContent="cascading style sheet";
+// items[3].textContent="application";
+// items[3].style.fontWeight="bold";
+// items[3].style.fontWeight="crimson";
+
+// for (i=0;i<items.length;i++)
+// {
+//     items[i].style.background="yellow";
+// }
+// console.log(items);
+
+//elementByTagName
+
+// var li=document.getElementsByTagName("li");
+// li[1].textContent="cascading style sheet"
+// li[3].textContent="application"
+// li[1].style.fontWeight="bold"
+// li[3].style.color="crimson";
+
+// for(i=0;i<li.length;i++)
+// {
+//     li[i].style.backgroundColor="yellow"
+// }
+// console.log(li);
+
+//queryselector
+
+// var headline=document.querySelector("#tagline");
+// headline.style.color="blue";
+
+// var items=document.querySelector(".list-items");
+// items.style.backgroundColor="pink";
+
+// console.log(headline);
+
+//queryselectorAll
+
+
+ var headline=document.querySelectorAll("#tagline");
+ headline.style.color="blue";
+
+var items=document.querySelector(".list-items");
+items.style.backgroundColor="pink";
+
+console.log(headline);
+
+
+
+
+
+
+
+
+
+
