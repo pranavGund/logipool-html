ac8a040da97b90d1a08c3bd7290b382455a3d2d6
= <=>
2023-06-29 12:23:39 -0700

    commit file2.js

---
+++ Dom/file3.js
@@ -0,0 +1,27 @@
+//    create element 
+var newDiv = document.createElement('div');
+// add class 
+newDiv.className = 'box';
+// add id 
+newDiv.id = 'textbox';
+
+// add content 
+ var newDivText = document.createTextNode('welcome to css');
+
+
+console.log(newDiv);
+console.log(newDivText);
+//add content to div 
+
+ newDiv.appendChild(newDivText);
+
+ console.log(newDiv);
+
+
+var container = document.querySelector('#content');
+
+newDiv.style.color ='crimson';
+newDiv.style.backgroundColor = 'yellow';
+
+ var form = document.querySelector('form');
+ container.insertBefore(newDiv,form);
