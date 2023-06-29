954c2e8a9af1217245c9740afaf4207639c1c1ee
= <=>
2023-06-29 12:24:09 -0700

    commit file2.js

---
+++ Dom/file2.js
@@ -0,0 +1,10 @@
+ElementnyId
+
+var headerTitle=document.getElementById("header-title")
+headerTitle.textContent="welcome To";
+headerTitle.innerHTML="Welcome To infotech"
+headerTitle.innerText="<h1>hello</h1>";
+console.log(headerTitle);
+
+var header=document.getElementById("header")
+header.style.borderBottom="10px solid blue";
