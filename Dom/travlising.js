6d8ff74f13a3a004aacc51c31ea3c23d7cc28bbd
= <=>
2023-07-04 11:19:44 -0700

    commit travlising.js

---
+++ Dom/travlising.js
@@ -0,0 +1,31 @@
+// var NewElement=document.createElement("li")
+// var NewText=document.createTextNode("Welcome")
+// NewElement.appendChild(NewText)
+// target=document.getElementById("item")
+// var OldElement=target.children[3]
+// console.log(OldElement);
+// target.replaceChild(OldElement)
+
+
+
+
+// var element=document.getElementById("remove")
+// var parent=element.parentElement;
+// console.log(parent);
+
+
+// var element=document.getElementById("remove")
+// var parent=element.firstElementChild;
+// console.log(parent);
+
+// var element=document.getElementById("remove")
+// var parent=element.lastElementChild;
+// console.log(parent);
+
+// var element=document.getElementById("remove")
+// var parent=element.previousElementSibling;
+// console.log(parent);
+
+var element=document.getElementById("remove")
+var parent=element.nextElementSibling;
+console.log(parent);
