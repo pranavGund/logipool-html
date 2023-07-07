22ad382bca8925e9d60018ee16ffd49939c53bf6
= <=>
2023-07-07 11:46:17 -0700

    commit dypatil.png

---
+++ Dom/events/practice.js
@@ -0,0 +1,16 @@
+
+ document.getElementById("img1").addEventListener("click",function btn(){
+       document.getElementById("img1").style.filter="grayscale(0%)"
+  })
+
+ document.getElementById("img2").addEventListener("mouseover",function btnClick(){
+     document.getElementById("img2").style.filter="grayscale(0%)"
+ })
+
+ document.getElementById("img3").addEventListener("mouseup",function click(){
+    document.getElementById("img3").style.filter="grayscale(0%)"
+ })
+
+ document.getElementById("img4").addEventListener("mouseout",function btn1(){
+    document.getElementById("img4").style.filter="grayscale(0%)"
+ })
